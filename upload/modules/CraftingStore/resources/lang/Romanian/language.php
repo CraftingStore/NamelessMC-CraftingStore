14ad11a268ff96518bd1e891af4e3a2e173713c5
BaxAndrei <bax@baxandrei.ro>
2021-02-16 22:05:37 +0200

    Create language.php

---
+++ upload/modules/CraftingStore/resources/lang/Romanian/language.php
@@ -0,0 +1,21 @@
+<?php
+
+$language_version = '2.0.0-pr8';
+
+$language = [
+    LanguageEnum::CRAFTING_STORE => 'CraftingStore',
+    LanguageEnum::NO_PACKAGES => 'Nu a fost gasit nici un pachet.',
+    LanguageEnum::SETTINGS => 'Setari',
+    LanguageEnum::SERVER_KEY => 'Cheie server',
+    LanguageEnum::SERVER_KEY_INFO => 'Poti gasi cheia serverului tau la sectiunea "Servers" pe CraftingStore.',
+    LanguageEnum::STORE_INDEX_CONTENT => 'Continut pagina principala magazin',
+    LanguageEnum::STORE_PATH => 'Cale magazin',
+    LanguageEnum::FORCE_SYNC => 'Sincronizare magazin',
+    LanguageEnum::HOME => 'Acasa',
+    LanguageEnum::BUY => 'Cumpara',
+    LanguageEnum::STORE => 'Magazin',
+    LanguageEnum::VIEW_REMOTE_STORE => 'Vezi tot magazinul',
+    LanguageEnum::UPDATED => 'Actualizat cu succes.',
+    LanguageEnum::SYNCED => 'Sincronizare completa.',
+    LanguageEnum::INVALID_KEY => 'Cheia API specificata nu este valida.',
+];
