c77364b4077dd54fb8fa19aa4590b669f5de4672
ItsLynix <71310187+ItsLynix@users.noreply.github.com>
2021-07-08 01:12:40 +0200

    Add swedish language
    I have added the translations for the Swedish language, which should automatically enable if user has enabled SwedishSE as language in NamelessMC

---
+++ upload/modules/CraftingStore/resources/lang/SwedishSE/language.php
@@ -0,0 +1,21 @@
+<?php
+
+$language_version = '2.0.0-pr7';
+
+$language = [
+    LanguageEnum::CRAFTING_STORE => 'CraftingStore',
+    LanguageEnum::NO_PACKAGES => 'Inga paket hittades.',
+    LanguageEnum::SETTINGS => 'Inställningar',
+    LanguageEnum::SERVER_KEY => 'Server Nyckel',
+    LanguageEnum::SERVER_KEY_INFO => 'Du kan hitta dina server nycklar på Server sektionen hos CraftingStore.',
+    LanguageEnum::STORE_INDEX_CONTENT => 'Butik Hemside Innehåll',
+    LanguageEnum::STORE_PATH => 'Butik sökväg',
+    LanguageEnum::FORCE_SYNC => 'Synka Butik',
+    LanguageEnum::HOME => 'Hem',
+    LanguageEnum::BUY => 'Köp',
+    LanguageEnum::STORE => 'Butik',
+    LanguageEnum::VIEW_REMOTE_STORE => 'Visa full butik',
+    LanguageEnum::UPDATED => 'Uppdateringen lyckades',
+    LanguageEnum::SYNCED => 'Synkronisering lyckades',
+    LanguageEnum::INVALID_KEY => 'API Nyckeln är ogiltig.',
+];
