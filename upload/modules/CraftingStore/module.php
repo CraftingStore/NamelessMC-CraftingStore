8d8eaf7448b4d174467d5a355a6e2bb8f94b1775
Tim Kwakman <tim@cloudprotected.net>
2021-07-27 00:10:37 +0200

    Bump version

--- upload/modules/CraftingStore/module.php
+++ upload/modules/CraftingStore/module.php
@@ -58,7 +58,7 @@ class CraftingStoreModule extends Module
 
         $name = 'CraftingStore';
         $author = 'CraftingStore';
-        $moduleVersion = '1.6';
+        $moduleVersion = '1.7';
         $namelessVersion = '2.0.0-pr10';
 
         parent::__construct($this, $name, $author, $moduleVersion, $namelessVersion);
